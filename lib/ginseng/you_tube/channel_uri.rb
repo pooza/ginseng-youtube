47b6ba42b11d7de696af6906569595caa42b31c0
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-08-09 18:05:52 +0900

    add: ChannelURI

---
+++ lib/ginseng/you_tube/channel_uri.rb
@@ -0,0 +1,37 @@
+module Ginseng
+  module YouTube
+    class ChannelURI < Ginseng::URI
+      include Package
+
+      def initialize(options = {})
+        super
+        @service = you_tube_service_class.new
+      end
+
+      def id
+        return query_values['v']
+      rescue
+        return nil
+      end
+
+      def data
+        @data ||= @service.lookup_video(id)
+        return @data
+      end
+
+      def title
+        return data.dig('snippet', 'title')
+      end
+
+      def channel_title
+        return data.dig('snippet', 'channelTitle')
+      end
+
+      def channel_id
+        return data.dig('snippet', 'channelId')
+      end
+
+      alias channnel channel_title
+    end
+  end
+end
