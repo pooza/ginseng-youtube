401a66e5dc21f00f181f9413f83841a29d83c61b
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-08-09 18:06:03 +0900

    update: ServiceTest

--- test/service_test.rb
+++ test/service_test.rb
@@ -8,9 +8,10 @@ module Ginseng
       def test_lookup_video
         return if Environment.ci?
         video = @service.lookup_video('uFfsTeExwbQ')
+
         assert_kind_of(Hash, video)
-        assert(video.present?)
-        assert_equal(video['snippet']['title'], '【キラキラ☆プリキュアアラモード】後期エンディング 「シュビドゥビ☆スイーツタイム」 （歌：宮本佳那子）')
+        assert_predicate(video, :present?)
+        assert_equal('【キラキラ☆プリキュアアラモード】後期エンディング 「シュビドゥビ☆スイーツタイム」 （歌：宮本佳那子）', video['snippet']['title'])
       end
     end
   end
